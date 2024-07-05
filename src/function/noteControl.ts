6bf22aea33555fd9e5417afd8a2b0fdafa32e3e8
yosufuturu <yosufuturu@gmail.com>
2024-07-05 22:44:02 +0900

    add adNote function

--- src/function/noteControl.ts
+++ src/function/noteControl.ts
@@ -1,18 +1,33 @@
 
 import { noteParam } from "../types";
 
-export function addNote(){
+export function addNote(value: string,
+        userNotesList: Array<noteParam>,
+        setUserNotesList: React.Dispatch<React.SetStateAction<Array<noteParam>>>
+){
+        // create new note 
+        const newNote :noteParam = {
+            id: userNotesList.length,
+            text: value,
+            pinState: false,
+            date: new Date()
+        };
+
+        const nextUserNotesList: Array<noteParam> = [...userNotesList, newNote];
+        setUserNotesList(nextUserNotesList);
+
+        // save list data
+        saveNotesList(JSON.stringify(nextUserNotesList));
+
+    }
     
+export function saveNotesList(saveData: string) {
+        
+        // save list data to local storage
+        localStorage.setItem('key', saveData);
 }
 
-export const saveNotesList = (saveData: string) => {
-
-    // const saveData = JSON.stringify(userNotesList)
-    console.log(saveData)
-    localStorage.setItem('key', saveData);
-}
-
-export const getNotesList = () => {
+export function getNotesList() {
     
     // Get data from local storage
     const jsonData = localStorage.getItem('key');
@@ -30,19 +45,19 @@ export const getNotesList = () => {
 // Initial Template
 const initData: Array<noteParam> = [
     {
-        id: 1,
+        id: 0,
         text: "Wellcome to StickyNotes",
         pinState: true,
         date: new Date()
     },
     {
-        id: 2,
+        id: 1,
         text: "The three buttons below allow pinning, copying, and deleting, respectively.",
         pinState: false,
         date: new Date()
     },
     {
-        id: 3,
+        id: 2,
         text: "Shige-san is a genius",
         pinState: false,
         date: new Date()
