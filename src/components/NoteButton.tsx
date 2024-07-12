d46f74b4d6557fdbb9095938d051e22eb36d10a0
yosufuturu <yosufuturu@gmail.com>
2024-07-12 15:39:25 +0900

    modified props

--- src/components/NoteButton.tsx
+++ src/components/NoteButton.tsx
@@ -1,50 +1,71 @@
-import { noteParam } from './../types'
-import { changePinState, copyNote, eraseNote } from '../function/noteControl'
+import React from "react";
+import { noteParam, updateNotesFunc, noteFunc } from './../types'
+// import { changePinState, copyNote, eraseNote } from '../function/noteControl'
 import pinImg from './assets/pin.svg'
 import copyImg from './assets/copy.svg'
 import dustImg from './assets/dustbox.svg'
 import './NoteButton.css'
 
-export const PinBtn = ({index, userNotesList, setUserNotesList}:
-    {
-        index: number
-        userNotesList: Array<noteParam>,
-        setUserNotesList: React.Dispatch<React.SetStateAction<Array<noteParam>>>
+export const PinBtn = ({
+    id,
+    userNotesList,
+    updateUserNotesList,
+    showList,
+    changePinState
+}:{
+    id: number
+    userNotesList: noteParam[],
+    updateUserNotesList: updateNotesFunc,
+    showList: noteParam[],
+    changePinState: noteFunc
     }) => {
-
+        
+        const noteValue: noteParam|undefined = showList.find((e)=>e.id===id);
+        if (undefined===noteValue) return
     return(
-        <button className={`noteBtn pinBtn pinState-${userNotesList[index].pinState}`} 
-        onClick={()=>{changePinState(index, userNotesList, setUserNotesList)}}>
+        <button className={`noteBtn pinBtn pinState-${noteValue.pinState}`} 
+        onClick={()=>{changePinState(id, userNotesList, updateUserNotesList)}}
+        >
             <img src={pinImg} className='pinIcon ButtonIcon' />
         </button>
     );
 };
 
-export const CopyBtn = ({index, userNotesList, setUserNotesList}:
-    {
-        index: number
-        userNotesList: Array<noteParam>,
-        setUserNotesList: React.Dispatch<React.SetStateAction<Array<noteParam>>>
-    }) => { 
+export const CopyBtn = ({
+    id,
+    userNotesList,
+    updateUserNotesList,
+    copyNote
+}:{
+    id: number
+    userNotesList: noteParam[],
+    updateUserNotesList: updateNotesFunc,
+    copyNote: noteFunc
+}) => {
 
     return(
         <button className={`noteBtn copyBtn`}
-        onClick={()=>{copyNote(index, userNotesList, setUserNotesList)}}>
+        onClick={()=>{copyNote(id, userNotesList, updateUserNotesList)}}>
             <img src={copyImg} className='copyIcon ButtonIcon' />
         </button>
     );
 };
 
-export const DustBtn = ({index, userNotesList, setUserNotesList}:
-    {
-        index: number
-        userNotesList: Array<noteParam>,
-        setUserNotesList: React.Dispatch<React.SetStateAction<Array<noteParam>>>
-    }) => {
+export const DustBtn = ({
+    id,
+    userNotesList,
+    updateUserNotesList,
+    eraseNote
+}:{
+    id: number
+    userNotesList: noteParam[],
+    updateUserNotesList: updateNotesFunc,
+    eraseNote: noteFunc
+}) => {
 
     return(
         <button className={`noteBtn dustBtn`} 
-        onClick={()=>{eraseNote(index, userNotesList, setUserNotesList)}} >
+        onClick={()=>{eraseNote(id, userNotesList, updateUserNotesList)}} >
             <img src={dustImg} className='dustIcon ButtonIcon' />
         </button>
     );
