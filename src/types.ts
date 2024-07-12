76ff0dfc44e05568ffea60690527d455c7578c4f
yosufuturu <yosufuturu@gmail.com>
2024-07-12 14:28:13 +0900

    add some types

--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,26 @@ export type noteParam = {
     pinState: boolean;
     date: Date;
 }
+
+export type updateNotesFunc = (
+    newNotes: noteParam[]
+) => void;
+
+export type noteFunc = (
+    id: number,
+    notesList: noteParam[],
+    updateNotesList: updateNotesFunc
+) => void;
+
+export type editFunc = (
+    text:string,
+    index:number,
+    userNotesList: noteParam[],
+    updateUserNotesList: updateNotesFunc
+) => void;
+
+export type addFunc = (
+    text: string,
+    notesList: noteParam[],
+    updateNotesList: updateNotesFunc
+) => void;
