b5e27ae0b2340f4815c6e15904a918f7db91dc34
yosufuturu <yosufuturu@gmail.com>
2024-07-12 15:38:05 +0900

    modified props

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,25 +1,55 @@
-import {PinBtn, CopyBtn, DustBtn} from './NoteButton'
-import { editNote } from '../function/noteControl';
-import { noteParam } from "../types";
+import React from "react";
+import { noteParam, updateNotesFunc, editFunc, noteFunc } from "../types";
+import { PinBtn, CopyBtn, DustBtn } from './NoteButton'
+// import { editNote } from '../function/noteControl';
 import './note.css';
 
 
-export const Note = ({userNotesList, setUserNotesList}:
-    {userNotesList: Array<noteParam>,
-        setUserNotesList: React.Dispatch<React.SetStateAction<Array<noteParam>>>
-    }) => {
+export const Note = ({
+    userNotesList,
+    updateUserNotesList,
+    showList,
+    editNote,
+    changePinState,
+    copyNote,
+    eraseNote
+}:{
+    userNotesList: noteParam[],
+    updateUserNotesList: updateNotesFunc,
+    showList: noteParam[],
+    editNote: editFunc
+    changePinState: noteFunc,
+    copyNote: noteFunc,
+    eraseNote: noteFunc
+}) => {
     
 
-    const notes = userNotesList.map(((e,i)=>{
+    const notes = showList.map(((e)=>{
         return(
         <div key={e.id} className={`note pin-${e.pinState}`}>
             <textarea className='noteArea' defaultValue={e.text}
-            onChange={(event)=>{editNote(event.target.value,i,userNotesList,setUserNotesList)}}
+            // onChange={(event)=>{editNote(event.target.value, i, userNotesList, updateUserNotesList)}}
             ></textarea>
             <div className='noteButtonContainer'>
-                <PinBtn index={i} userNotesList={userNotesList} setUserNotesList={setUserNotesList} />
-                <CopyBtn index={i} userNotesList={userNotesList} setUserNotesList={setUserNotesList} />
-                <DustBtn index={i} userNotesList={userNotesList} setUserNotesList={setUserNotesList} />
+                <PinBtn id={e.id}
+                userNotesList={userNotesList}
+                updateUserNotesList={updateUserNotesList}
+                showList={showList}
+                changePinState={changePinState}
+                />
+                <CopyBtn
+                id={e.id}
+                userNotesList={userNotesList}
+                updateUserNotesList={updateUserNotesList}
+                copyNote={copyNote}
+                />
+
+                <DustBtn 
+                id={e.id}
+                userNotesList={userNotesList}
+                updateUserNotesList={updateUserNotesList}
+                eraseNote={eraseNote}
+                />
             </div>
         </div>
         );
