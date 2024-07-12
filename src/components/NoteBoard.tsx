c375709abb17efe681baa442cf40c20bf10af986
yosufuturu <yosufuturu@gmail.com>
2024-07-12 16:06:44 +0900

    modified props

--- src/components/NoteBoard.tsx
+++ src/components/NoteBoard.tsx
@@ -1,23 +1,60 @@
-import {useState} from "react";
+import React, { useState } from "react";
 import { Note } from './Note';
 import { AddNoteContainer } from "./AddNote";
-import { noteParam } from "../types";
+import { noteParam, updateNotesFunc, editFunc, noteFunc, addFunc } from "../types";
 import { getNotesList } from './../function/noteControl';
 import './NoteBoard.css';
 
-export const NoteBoard = () => {
+
+
+export const NoteBoard = ({
+    userNotesList,
+    updateUserNotesList,
+    showList,
+    // updateShowList,
+    // searchWord,
+    changePinState,
+    copyNote,
+    eraseNote,
+    addNote,
+    editNote
+}:{
+    userNotesList: noteParam[],
+    updateUserNotesList: updateNotesFunc,
+    showList:noteParam[],
+    // updateShowList: updateNotesFunc,
+    // searchWord: string,
+    changePinState: noteFunc,
+    copyNote: noteFunc,
+    eraseNote: noteFunc,
+    addNote: addFunc,
+    editNote: editFunc
+}) => {
 
     // Get memo data
-    const notesList: Array<noteParam> = getNotesList();
+    // const notesList: Array<noteParam> = getNotesList();
 
-    const [userNotesList,setUserNotesList] = useState<Array<noteParam>>(notesList);
+    // const [userNotesList,setUserNotesList] = useState<Array<noteParam>>(notesList);
+
+        //console.log(searchWord);
+
     return (
         <>
-        <AddNoteContainer userNotesList={userNotesList} setUserNotesList={setUserNotesList} />
+        <AddNoteContainer
+        userNotesList={userNotesList}
+        updateUserNotesList={updateUserNotesList}
+        addNote={addNote}
+        />
         <div className="noteBoard">
-        <Note userNotesList={userNotesList} setUserNotesList={setUserNotesList} />
-
-
+            <Note
+            userNotesList={userNotesList}
+            updateUserNotesList={updateUserNotesList}
+            showList={showList}
+            editNote={editNote}
+            changePinState={changePinState}
+            copyNote={copyNote}
+            eraseNote={eraseNote}
+            />
         </div>
         </>
     );
