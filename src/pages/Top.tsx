c4b9e87d861b579340db64e4732b9ebc6724d8a2
yosufuturu <yosufuturu@gmail.com>
2024-07-12 15:01:55 +0900

    modified imports

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Header } from '../components/Header';
 import { NoteBoard } from '../components/NoteBoard';
-import { inputControl } from '../function/inputControl'
 import { noteControl } from '../function/noteControl'
 
 export const Top = () => {
