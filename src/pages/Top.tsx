dc28ddd63e215b114c659895d353d641f07cd6bf
yosufuturu <yosufuturu@gmail.com>
2024-07-12 14:36:41 +0900

    add page component

---
+++ src/pages/Top.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Header } from '../components/Header';
+import { NoteBoard } from '../components/NoteBoard';
+import { inputControl } from '../function/inputControl'
+import { noteControl } from '../function/noteControl'
+
+export const Top = () => {
+
+        const {
+            userNotesList, updateUserNotesList,
+            showList, updateShowList,
+            searchWord, updateSearchWord,
+            changePinState, copyNote, eraseNote,
+            addNote,
+            editNote
+        } = noteControl();
+        
+
+    return (
+        <>
+        <Header updateSearchWord={updateSearchWord} />
+        <NoteBoard
+            userNotesList={userNotesList}
+            updateUserNotesList={updateUserNotesList}
+            showList={showList}
+            // updateShowList={updateShowList}
+            // searchWord={searchWord}
+            changePinState={changePinState}
+            copyNote={copyNote}
+            eraseNote={eraseNote}
+            addNote={addNote}
+            editNote={editNote} 
+            />
+        </>
+    );
+};
